f8b036234f80db8000f3a1ac016a953d6471c4f7
manasa17300 <35917713+manasa17300@users.noreply.github.com>
2018-02-12 16:29:30 +0530

    Add files via upload

---
+++ feb12.py
@@ -0,0 +1,48 @@
+ int(1+2)
+3 #int output
+>>> int("111"+"222")
+111222 #int output
+
+>>> float(11//10)
+1.0 #float output
+>>> float(11/10)
+1.1 #float output
+
+>>> complex(2,3)
+(2+3j) #complex output
+>>> complex(50,60)
+(50+60j) #complex output
+
+>>> L=[10,20,30,"hi",50]     
+>>> L[4] 
+50  #list output
+>>> L[3]
+'hi'  #list output
+
+>>> tup1=("aaa","bbb","ccc")
+>>> tup2=(1,2,3)
+>>> tup1[0]
+'aaa' #tuple output
+>>> tup2[2]
+3 #tuple output
+
+
+>>> D={0:1,1:12,3:34}
+>>> D[0]
+1 #dictionary output
+>>> D[3]
+34 #dictionary output
+
+
+>>> var1 = 'Hello World!'
+>>> var1[0]
+'H' #string output
+>>> var1[7]
+'o'  #string output
+
+
+>>> 3+3==3*2
+True #boolean output
+>>> 3*3==3+2
+False  #boolean output
+
