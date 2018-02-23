210451129ac14d15204e433157db1750e4bbe243
manasa17300 <35917713+manasa17300@users.noreply.github.com>
2018-02-23 09:37:01 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,22 @@
+#function
+
+print("using add function")
+def add(x,y):
+    return x+y
+print((add(2,7)))
+
+print("using sub function")
+def sub(x,y):
+    return x-y
+print((sub(7,2)))
+
+print("using mul function")
+def mul(x,y):
+    return x*y
+print((mul(7,2)))
+
+
+print("using div function")
+def div(x,y):
+    return x/y
+print((div(25,5)))
