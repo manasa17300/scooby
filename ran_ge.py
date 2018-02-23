b164ebe7803ddeaa3da53aa4f3dfd222c99b395a
manasa17300 <35917713+manasa17300@users.noreply.github.com>
2018-02-23 09:37:15 +0530

    Add files via upload

---
+++ ran_ge.py
@@ -0,0 +1,32 @@
+
+
+#range
+print("using range")
+for i in range(10):
+    print(i)
+
+
+print("using range part 2")
+for i in range(50,101):
+    print(i)
+    
+
+print("using range part 3,even numbers")
+for i in range(2,20,2):
+    print(i)
+
+print("using range part 4, odd numbers")
+for i in range(3,21,3):
+    
+    print(i)
+    
+print("using range part 5")
+for i in range(0,11,2):
+    print(i)
+    
+
+print("using range part 6")
+for i in range(0,50,4):
+    print(i)
+
+
